72c6ef387e334c7c4e6894f627d545150ebf8cb1
Harsh-Joshi-16 <joshiharshjayesh@gmail.com>
2025-05-15 11:35:40 +0530

    More patterns

--- Patterns/Pattern18.java
+++ Patterns/Pattern18.java
@@ -4,7 +4,7 @@ public class Pattern18 {
     public static void main(String[] args) {
         char ch = 'A' + 4;
         for (int row = 0; row < 5; row++) {
-            char current = (char)(ch - row);
+            char current = (char) (ch - row);
             for (char cha = current; cha <= ch; cha++) {
                 System.out.print(cha + " ");
             }
--- Patterns/Pattern18_1.java
+++ Patterns/Pattern18_1.java
@@ -4,7 +4,7 @@ public class Pattern18_1 {
     public static void main(String[] args) {
         char ch = 'A' + 4;
         for (int row = 0; row < 5; row++) {
-            char current = (char)(ch - row);
+            char current = (char) (ch - row);
             for (char cha = ch; cha >= current; cha--) {
                 System.out.print(cha + " ");
             }
