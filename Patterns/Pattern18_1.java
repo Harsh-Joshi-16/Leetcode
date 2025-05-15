23788134016c3781d2f05287c5fd94596106902d
Harsh-Joshi-16 <joshiharshjayesh@gmail.com>
2025-05-15 11:34:06 +0530

    More patterns

--- Patterns/Pattern18_1.java
+++ Patterns/Pattern18_1.java
@@ -1,11 +1,11 @@
 package Patterns;
 
-public class Pattern18 {
+public class Pattern18_1 {
     public static void main(String[] args) {
         char ch = 'A' + 4;
         for (int row = 0; row < 5; row++) {
             char current = (char)(ch - row);
-            for (char cha = current; cha <= ch; cha++) {
+            for (char cha = ch; cha >= current; cha--) {
                 System.out.print(cha + " ");
             }
             System.out.println();
